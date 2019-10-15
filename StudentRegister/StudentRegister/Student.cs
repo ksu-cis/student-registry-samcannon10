42957fe80281d9170e53b779166c9a39da894bfe
Sam Cannon <samcannon10@ksu.edu>
2019-10-15 15:05:51 -0500

    Data Binding Example 10/15/2019

--- StudentRegister/StudentRegister/Student.cs
+++ StudentRegister/StudentRegister/Student.cs
@@ -10,19 +10,47 @@ namespace StudentRegister
     /// <summary>
     /// A class representing a student
     /// </summary>
-    public class Student
+    public class Student : INotifyPropertyChanged
     {
         private List<CourseResult> courseHistory;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string first;
+        private string last;
+
+        private void NotifyOfPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         /// <summary>
         /// Gets and sets the first name
         /// </summary>
-        public string First { get; set; }
+        public string First {
+            get { return first; }
+            set
+            {
+                if(first != value)
+                {
+                    first = value;
+                    NotifyOfPropertyChanged("First");
+                }
+            }
+        }
 
         /// <summary>
         /// Gets and sets the last name
         /// </summary>
-        public string Last { get; set; }
+        public string Last {
+            get { return last; }
+            set
+            {
+                if (last != value)
+                {
+                    last = value;
+                    NotifyOfPropertyChanged("Last");
+                }
+            }
+        }
 
         /// <summary>
         /// Gets the course history
@@ -86,5 +114,17 @@ namespace StudentRegister
             courseHistory = new List<CourseResult>();
         }
 
+
+        public override string ToString()
+        {
+            return $"{Last}, {First} ({GPA})";
+        }
+
+        public void CourseComplete(string name, uint hours, Grade grade, string semester)
+        {
+            courseHistory.Add(new CourseResult(name, hours, grade, semester));
+            NotifyOfPropertyChanged("GPA");
+            NotifyOfPropertyChanged("CourseHistory");
+        }
     }
 }
